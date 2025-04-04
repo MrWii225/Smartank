87266a3abda283ea8204b3f170c977d771b7a5ee
MrWii225 <smithzachary1129@gmail.com>
2025-04-04 09:39:17 -0500

    Create dictionaries.py
    Created dictionaries.py and implemented three dictionaries, Celcius temp, Fahrenheit temp, and the basic fish list

---
+++ dictionaries.py
@@ -0,0 +1,40 @@
+
+
+
+fish = {
+    1:"Goldfish",
+    2:"Guppy",
+    3:"Zebrafish",
+    4:"Tetra",
+    5:"Minnow",
+    6:"Pea Puffer",
+    7:"Barb",
+    8:"Swordtail",
+    9:"Dwarf Gourami"
+}
+
+
+ideal_temp_f = {
+    "Goldfish":"68°F-74°F",
+    "Guppy":"72°F-82°F",
+    "Zebrafish":"79°F-83°F",
+    "Tetra":"75°F-80°F",
+    "Minnow":"60°F-70°F",
+    "Pea Puffer":"74°F-82°F",
+    "Barb":"75°F-80°F",
+    "Swordtail":"64°F-82°F",
+    "Dwarf Gourami":"75°F-80°F"
+}
+
+
+ideal_temp_c = {
+    "Goldfish":"20°C-23°C",
+    "Guppy":"22°C-28°C",
+    "Zebrafish":"26°C-28°C",
+    "Tetra":"24°C-27°C",
+    "Minnow":"16°C-21°C",
+    "Pea Puffer":"23°C-28°C",
+    "Barb":"24°C-27°C",
+    "Swordtail":"18°C-28°C",
+    "Dwarf Gourami":"24°C-27°C"
+}
