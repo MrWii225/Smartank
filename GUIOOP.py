ca78911f938a81184b7438920035d0180e521e91
MrWii225 <smithzachary1129@gmail.com>
2025-04-05 12:03:15 -0500

    Update GUIOOP.py

--- GUIOOP.py
+++ GUIOOP.py
@@ -1,6 +1,6 @@
 import tkinter as tk 
 
-class SmartankGUI(tk.tk):
+class SmartankGUI(tk.Tk):
     
     def __init__(self):
         super().__init__()
@@ -12,11 +12,11 @@ class SmartankGUI(tk.tk):
         
         self.frames = {}
          
-         for PageClass in (InitialPage, FishionaryPage, GoldfishData, GuppyData, ZebrafishData, TetraData, MinnowData, PeaPufferData, BarbData, SwordtailData, DwarfGouramiData):
-             page_name = PageClass.__name__
-             frame = PageClass(parent=container, controller=self)
-             self.frames[page_name] = frame 
-             frame.grid(row=0, column=0, sticky="nsew") 
+        for PageClass in (InitialPage, FishionaryPage, GoldfishData, GuppyData, ZebrafishData, TetraData, MinnowData, PeaPufferData, BarbData, SwordtailData, DwarfGouramiData):
+            page_name = PageClass.__name__
+            frame = PageClass(parent=container, controller=self)
+            self.frames[page_name] = frame 
+            frame.grid(row=0, column=0, sticky="nsew") 
         
         self.show_frame("InitialPage")
     
@@ -44,3 +44,9 @@ class FishionaryPage(tk.Frame):
         tk.Button(self, text="Swordtail", command=lambda: controller.show_frame("SwordtailData")).grid(row=1, column=2)
         tk.Button(self, text="Dwarf Gourami", command=lambda: controller.show_frame("DwarfGouramiData")).grid(row=2, column=2)
         tk.Button(self, text="Go Back", command=lambda: controller.show_frame("InitialPage")).grid(row=3, column= 1)
+
+
+
+
+smartankgui = SmartankGUI()
+smartankgui.mainloop()
