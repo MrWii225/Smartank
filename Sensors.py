8b312af67d7a0b6e3a79c99b4e9657bbbcbb42bf
MrWii225 <smithzachary1129@gmail.com>
2025-05-01 18:04:00 -0500

    added temp sensor

--- Sensors.py
+++ Sensors.py
@@ -2,6 +2,7 @@ import time
 import board
 import busio 
 import digitalio 
+from w1thermsensor import W1ThermSensor
 
 from adafruit_mcp3xxx.mcp3008 import MCP3008, P0 
 from adafruit_mcp3xxx.analog_in import AnalogIn
@@ -16,6 +17,11 @@ mcp = MCP3008(spi, cs)
 # Use CH0 for the pH sensor
 ph_channel = AnalogIn(mcp, P0)
 
+#Temperature sensor setup
+temp_sensor = W1ThermSensor()
+temperature_c = temp_sensor.get_temperature()
+temperature_f = (temperature_c * 9/5) + 32 
+
 def voltage_to_ph(voltage):
     #Calibrate with calibration fluids
     ph = (7.78 * voltage - 8.54)
@@ -24,6 +30,7 @@ def voltage_to_ph(voltage):
 while True:
     voltage = ph_channel.voltage
     raw_value = ph_channel.value 
+    print(f"Temperature {temperature_f:.2f}")
     print(voltage_to_ph(voltage))
     print(f"Raw ADC Value: {raw_value}, Voltage: {voltage:.3f} V")
     time.sleep(1)
